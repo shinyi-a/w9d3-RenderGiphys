e7d82e4f2235fa30cef481a4124e41c394f2dd29
Shin Yi <contact.shinyi@gmail.com>
2021-11-11 18:46:52 +0800

    added deployment packages

--- src/Components/Gif.js
+++ src/Components/Gif.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-export default function Gif () {
+export default function Gif (props) {
+    const url=props;
     return (
         <div>
-            <img src="" alt=""/>
+            <img src={url} alt=""/>
         </div>
     )
 }
