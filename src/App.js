167909300b0c317581def82430772a82a3d75473
Shin Yi <contact.shinyi@gmail.com>
2021-11-11 18:39:53 +0800

    added gif to body

--- src/App.js
+++ src/App.js
@@ -3,14 +3,26 @@ import './App.css';
 import Gif from './Components/Gif';
 import Button from './Components/Button';
 
-function App() {
+function App(props) {
+
+  const [data, setData] = useState([]);
+  useEffect(()=>{
+    fetch('https://api.giphy.com/v1/gifs/random?api_key=YwpCRt35hzewLXvKfvWvt7hMxG6lQS4w&tag=&rating=pg')
+      .then((response) => response.json())
+      .then((gifs) => setData(gifs.data.id));
+  },[])
+
+  console.log(data);
+
+  let gifurl = "https://i.giphy.com/media/"+data+"/giphy.webp";
+
   return (
     <div className="App">
       <h1>Giphy</h1>
-      <br />
       <Button />
       <br />
       <h2>Use Giphy to pull some giphs</h2>
+      <img src={gifurl} />
       <Gif />
     </div>
   );
